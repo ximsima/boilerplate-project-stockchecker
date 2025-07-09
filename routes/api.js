90e21f65dd7d3e3bc0d98d93e005e528ff480a02
ximsima <ximsima@ioiio.cc>
2025-07-09 19:35:26 +0700

    error

--- routes/api.js
+++ routes/api.js
@@ -2,14 +2,35 @@
 
 module.exports = function (app) {
 
+  let likeArray = [{
+    stock: "AMD",
+    likes: new Set(["::ffff:127.0.0.1"])
+  }];
+
   app.route('/api/stock-prices')
     .get(async function (req, res) {
       const { stock } = req.query;
       const { like } = req.query;
       console.log("stock:", stock, "like:", like);
+      console.log("IP", req.ip, req.socket.remoteAddress);
 
       const stocks = Array.isArray(stock) ? stock : [stock];
       console.log("stocks", stocks);
+
+      const addLike = (stock) => {
+        const foundStock = likeArray.find(s => s.symbol === stock);
+        if (foundStock) {
+          foundStock.likes.add(req.ip)
+        } else {
+          likeArray.add({
+            stock: stock,
+            likes: new Set([req.ip])
+          });
+        }
+      }
+
+      if (like) stocks.forEach(s => addLike(s));
+      console.log(likeArray);
 
       const stockData = await Promise.all(
         stocks.map(async (stock) => {
