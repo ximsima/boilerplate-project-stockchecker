a396c112d012c0f4719ed113bad0e8ce236b50b7
ximsima <ximsima@ioiio.cc>
2025-07-09 15:27:43 +0700

    add resp

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,29 @@
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async function (req, res) {
+      const { stock } = req.query;
+      const { like } = req.query;
+      console.log("stock:", stock, "like:", like);
+
+      const stocks = Array.isArray(stock) ? stock : [stock];
+      console.log("stocks", stocks);
+
+      const stockData = await Promise.all(
+        stocks.map(async (stock) => {
+          const sData = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`);
+          const data = await sData.json();
+          return {
+            stock: data.symbol,
+            price: data.latestPrice,
+            likes: 0
+          };
+        })
+      );
+
+      const result = stockData.length < 2 ? stockData[0] : stockData;
+      console.log("result", result);
+      res.json(result);
     });
-    
+
 };
