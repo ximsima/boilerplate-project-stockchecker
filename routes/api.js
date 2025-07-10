33126e34b0baf5f9f2590c75b4dc59e93863fbd0
ximsima <ximsima@ioiio.cc>
2025-07-10 17:40:17 +0700

    with tests

--- routes/api.js
+++ routes/api.js
@@ -11,6 +11,7 @@ module.exports = function (app) {
     .get(async function (req, res) {
       const { stock } = req.query;
       const { like } = req.query;
+      console.log("================");
       console.log("stock:", stock, "like:", like);
       console.log("IP", req.ip, req.socket.remoteAddress);
 
@@ -18,11 +19,12 @@ module.exports = function (app) {
       console.log("stocks", stocks);
 
       const addLike = (stock) => {
-        const foundStock = likeArray.find(s => s.symbol === stock);
+        const foundStock = likeArray.find(s => s.stock === stock);
+        console.log("addLike/foundStock", foundStock);
         if (foundStock) {
           foundStock.likes.add(req.ip)
         } else {
-          likeArray.add({
+          likeArray.push({
             stock: stock,
             likes: new Set([req.ip])
           });
@@ -30,21 +32,34 @@ module.exports = function (app) {
       }
 
       if (like) stocks.forEach(s => addLike(s));
-      console.log(likeArray);
 
       const stockData = await Promise.all(
         stocks.map(async (stock) => {
           const sData = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`);
           const data = await sData.json();
+          let likeCount = 0;
+          const foundStock = likeArray.find(s => s.stock === data.symbol);
+          console.log("Promise/foundStock", foundStock);
+          if (foundStock) likeCount = foundStock.likes.size;
           return {
             stock: data.symbol,
             price: data.latestPrice,
-            likes: 0
+            likes: likeCount
           };
         })
       );
 
-      const result = stockData.length < 2 ? stockData[0] : stockData;
+      let result;
+      if (stockData.length > 1) {
+        stockData[0].rel_like = stockData[0].likes - stockData[1].likes;
+        stockData[1].rel_like = stockData[1].likes - stockData[0].likes;
+        delete stockData[0]['likes'];
+        delete stockData[1]['likes'];
+        result = stockData;
+      } else {
+        result = stockData[0];
+      }
+
       console.log("result", result);
       res.json(result);
     });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,65 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', function () {
+    test('Viewing one stock', function (done) {
+        chai.request(server)
+            .get('/api/stock-prices?stock=GOOG')
+            .end((err, res) => {
+                assert.equal(res.statusCode, 200);
+                const data = JSON.parse(res.text);
+                assert.equal(data.stock, "GOOG");
+                done(); // Signal Mocha that the asynchronous test is complete
+            });
+    });
 
+    test('Viewing one stock and liking it', function (done) {
+        chai.request(server)
+            .get('/api/stock-prices?stock=GOOG&like=true')
+            .end((err, res) => {
+                assert.equal(res.statusCode, 200);
+                const data = JSON.parse(res.text);
+                assert.equal(data.stock, "GOOG");
+                assert.equal(data.likes, 1);
+                done(); // Signal Mocha that the asynchronous test is complete
+            });
+    });
+
+    test('Viewing the same stock and liking it again', function (done) {
+        chai.request(server)
+            .get('/api/stock-prices?stock=GOOG&like=true')
+            .end((err, res) => {
+                assert.equal(res.statusCode, 200);
+                const data = JSON.parse(res.text);
+                assert.equal(data.stock, "GOOG");
+                assert.equal(data.likes, 1);
+                done(); // Signal Mocha that the asynchronous test is complete
+            });
+    });
+
+    test('Viewing two stocks', function (done) {
+        chai.request(server)
+            .get('/api/stock-prices?stock=GOOG&stock=AMD')
+            .end((err, res) => {
+                assert.equal(res.statusCode, 200);
+                const data = JSON.parse(res.text);
+                assert.equal(data[0].stock, "GOOG");
+                assert.equal(data[1].stock, "AMD");
+                done(); // Signal Mocha that the asynchronous test is complete
+            });
+    });
+
+    test('Viewing two stocks and liking them', function (done) {
+        chai.request(server)
+            .get('/api/stock-prices?stock=GOOG&stock=AMD&like=true')
+            .end((err, res) => {
+                assert.equal(res.statusCode, 200);
+                const data = JSON.parse(res.text);
+                assert.equal(data[0].stock, "GOOG");
+                assert.equal(data[1].stock, "AMD");
+                assert.equal(data[0].rel_like, 0);
+                assert.equal(data[1].rel_like, 0);
+                done(); // Signal Mocha that the asynchronous test is complete
+            });
+    });
 });
